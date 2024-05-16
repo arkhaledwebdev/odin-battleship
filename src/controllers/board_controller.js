f526a29226fd90b2f4487e4da2eb0c31fbdde1ad
arkhaledwebdev <arkhaledwebdev@gmail.com>
2024-05-16 16:23:52 +0300

    finish

--- src/controllers/board_controller.js
+++ src/controllers/board_controller.js
@@ -1,15 +1,37 @@
 function createBoard(name, player) {
   const board = document.getElementById(`${name}-board`);
+  board.replaceChildren();
 
   for (let i = 0; i < 100; i += 1) {
     const node = document.createElement("div");
     node.classList.add(`${name}-board-node`);
     node.dataset.index = i;
-    if (player.gameboard.board[i].ship !== null) {
+    if (player.gameboard.board[i].ship !== null && name !== "computer") {
       node.classList.add("has-ship");
     }
     board.appendChild(node);
   }
 }
 
-module.exports = { createBoard };
+function updateComputerBoard(playerBoard) {
+  const playerNodes = document.querySelectorAll(".player-board-node");
+  playerNodes.forEach((node) => {
+    const nodeIndex = node.dataset.index;
+    if (!node.classList.contains("selected")) {
+      if (playerBoard[nodeIndex].isHit) {
+        node.classList.add("selected");
+        const playerHitNode = document.createElement("div");
+        playerHitNode.classList.add("player-hit-node");
+        node.appendChild(playerHitNode);
+      }
+      if (playerBoard[nodeIndex].isMissed) {
+        node.classList.add("selected");
+        const playerMissedNode = document.createElement("div");
+        playerMissedNode.classList.add("player-missed-node");
+        node.appendChild(playerMissedNode);
+      }
+    }
+  });
+}
+
+module.exports = { createBoard, updateComputerBoard };
--- src/index.js
+++ src/index.js
@@ -1,41 +1,77 @@
 import "./style.css";
 import { initComputer, initPlayer } from "./controllers/player_controller";
-import { createBoard } from "./controllers/board_controller";
+import {
+  createBoard,
+  updateComputerBoard,
+} from "./controllers/board_controller";
 
-// let isPlayerTurn = true;
+let isPlayerTurn = true;
 let player = initPlayer();
 let computer = initComputer();
 createBoard("player", player);
 createBoard("computer", computer);
+document.getElementById("computer-board").classList.add("selected-turn");
 
-// const computerNodes = document.querySelectorAll(".computer-board-node");
-// computerNodes.forEach((node) => {
-//   node.addEventListener("click", () => {
-//     const nodeIndex = node.dataset.index;
-//     if (!node.classList.contains("selected")) {
-//       node.classList.add("selected");
-//       if (computer.gameboard.board[nodeIndex].ship !== null) {
-//         computer.gameboard.board[nodeIndex].isHit = true;
-//         computer.gameboard.board[nodeIndex].ship.hit();
-//         const computerHitNode = document.createElement("div");
-//         computerHitNode.classList.add("computer-hit-node");
-//         node.appendChild(computerHitNode);
-//       } else {
-//         computer.gameboard.board[nodeIndex].isMissed = true;
-//         const computerMissedNode = document.createElement("div");
-//         computerMissedNode.classList.add("computer-missed-node");
-//         node.appendChild(computerMissedNode);
-//       }
-//     }
-//   });
-// });
+function gameOver() {
+  const dialogText = document.getElementById("dialog-text");
+  dialogText.textContent = `player 1 has won`;
+  document.getElementById("game-over-dialog").showModal();
+}
+
+function switchTurn() {
+  if (player.isGameOver() || computer.isGameOver()) {
+    gameOver();
+  }
+  isPlayerTurn = !isPlayerTurn;
+  if (isPlayerTurn) {
+    document.getElementById("computer-board").classList.add("selected-turn");
+    document.getElementById("player-board").classList.remove("selected-turn");
+  } else {
+    document.getElementById("player-board").classList.add("selected-turn");
+    document.getElementById("computer-board").classList.remove("selected-turn");
+  }
+  if (!isPlayerTurn) {
+    computer.attackWithAi(player.gameboard);
+    updateComputerBoard(player.gameboard.board);
+    setTimeout(() => {
+      switchTurn();
+    }, 500);
+  }
+}
+
+function addClickListener() {
+  const computerNodes = document.querySelectorAll(".computer-board-node");
+  computerNodes.forEach((node) => {
+    node.addEventListener("click", () => {
+      const nodeIndex = node.dataset.index;
+      if (!node.classList.contains("selected") && isPlayerTurn) {
+        node.classList.add("selected");
+        if (computer.gameboard.board[nodeIndex].ship !== null) {
+          computer.gameboard.board[nodeIndex].isHit = true;
+          computer.gameboard.board[nodeIndex].ship.hit();
+          const computerHitNode = document.createElement("div");
+          computerHitNode.classList.add("computer-hit-node");
+          node.appendChild(computerHitNode);
+          switchTurn();
+        } else {
+          computer.gameboard.board[nodeIndex].isMissed = true;
+          const computerMissedNode = document.createElement("div");
+          computerMissedNode.classList.add("computer-missed-node");
+          node.appendChild(computerMissedNode);
+          switchTurn();
+        }
+      }
+    });
+  });
+}
+
+addClickListener();
 
 const resetButton = document.getElementById("start-button");
 resetButton.addEventListener("click", () => {
-  document.getElementById(`player-board`).replaceChildren();
-  document.getElementById(`computer-board`).replaceChildren();
   player = initPlayer();
   computer = initComputer();
   createBoard("player", player);
   createBoard("computer", computer);
+  addClickListener();
 });
